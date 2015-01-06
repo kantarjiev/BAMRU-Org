a00726865efd87429b56700e03c3e30ad2059b69
andyl <andy@r210.com>
2015-01-05 22:06:14 -0800

    first addition of setup scripts

---
+++ bin/setup_mac.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Setup Script for BAMRU.org Development
+
+# UNDER CONSTRUCTION
+
+# This script installs all the support software you need
+# should work on Macintosh
+
+# === Step 1: Install And Configure Git
+
+# === Step 3: Install Ruby 
+
+# === Step 4: Install Gems
+
+# === Step 5: 
