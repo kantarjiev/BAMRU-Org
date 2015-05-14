a7c7659be4766470aae5d1b037054535ab9ba83a
andyl <andy@r210.com>
2015-05-14 07:56:15 -0700

    added cron tasks

--- cron.rb
+++ cron.rb
@@ -1,16 +1,32 @@
 # Use this file to easily define all of your cron jobs.
 #
 # to generate cronfile : `whenever -f cron.rb --update-crontab`
+# to clear cronfile    : `whenever -f cron.rb --clear-crontab`
 # for help on whenever : `whenever -h`
 # to see cron settings : `crontab -l`
+# to edit the cronfile : `crontab -l`
 #
 # Learn more:
 # - http://github.com/javan/whenever  | cron processor
 # - http://en.wikipedia.org/wiki/Cron | cron instructions
 
-set :output, "/tmp/cron_mvcondo.log"
+set :output, "/tmp/bamru_org.log"
+
+cmd = "data:bnet:download data:bnet:refine site:build site:deploy"
 
 every 1.day, at: '5:00 am' do
-  rake "cl:pull cl:generate site:generate site:deploy"
+  rake cmd
+end
+
+every 1.day, at: '11:00 am' do
+  rake cmd
+end
+
+every 1.day, at: '5:00 pm' do
+  rake cmd
+end
+
+every 1.day, at: '11:00 pm' do
+  rake cmd
 end
 
--- lib/rake/cron.rake
+++ lib/rake/cron.rake
@@ -3,17 +3,20 @@ require_relative "../base"
 namespace :cron do
   desc "Initialize cron process"
   task :init do
-    log "cron:init - under construction"
+    log "cron:init"
+    system "whenever -f cron.rb --update-crontab"
   end
 
   desc "Remove cron process"
   task :remove do
-    log "cron:remove - under construction"
+    log "cron:remove"
+    system "whenever -f cron.rb --clear-crontab"
   end
 
   desc "Show cron status"
   task :status do
-    log "cron:status - under construction"
+    log "cron:status"
+    system "crontab -l"
   end
 end
 
