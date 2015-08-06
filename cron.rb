e040f80cf85816ab73857c490d851ac8ba68d125
andyl <andy@r210.com>
2015-08-05 17:10:16 -0700

    Added total:rebuild rake task

--- cron.rb
+++ cron.rb
@@ -13,10 +13,7 @@
 set :output, "/tmp/bamru_org.log"     # log file output
 set :environment_variable, "MM_ENV"   # sets MM_ENV to 'production'
 
-cmd_bnet = "data:bnet:download data:bnet:refine"
-cmd_gcal = "data:gcal:download data:gcal:refine data:gcal:sync"
-cmd_site = "site:build site:deploy_calendar"
-cmd = [cmd_bnet, cmd_gcal, cmd_site].join(' ')
+cmd = "total:rebuild"
 
 every 1.day, at: '5:00 am' do
   rake cmd
