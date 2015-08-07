f3e78b0deb29b455944a666c64ded6c54063ab73
andyl <andy@r210.com>
2015-08-07 07:44:59 -0700

    Test Travis CI with failing spec

--- spec/lib/event_spec.rb
+++ spec/lib/event_spec.rb
@@ -26,5 +26,6 @@ describe Event do
   describe "Instance Methods" do
     it { should respond_to :id                     }
     it { should respond_to :hash                   }
+    it { should respond_to :unknown                }
   end
 end
