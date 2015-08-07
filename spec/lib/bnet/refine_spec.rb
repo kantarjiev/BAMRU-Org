f860c6acc7f81e22d30351bdac703c4edd451277
andyl <andy@r210.com>
2015-08-06 21:56:44 -0700

    Reorganize specs to match directory layout

--- spec/lib/bnet/refine_spec.rb
+++ spec/lib/bnet/refine_spec.rb
@@ -13,9 +13,11 @@ describe CalData::Bnet::Refine do
     it { should respond_to :execute }
   end
 
-  describe "_events" do
-    it "returns an array" do
-      expect(subject.send(:events)).to be_an(Array)
+  if File.exist?('cal_data/bnet.csv')
+    describe "_events" do
+      it "returns an array" do
+        expect(subject.send(:events)).to be_an(Array)
+      end
     end
   end
 end
